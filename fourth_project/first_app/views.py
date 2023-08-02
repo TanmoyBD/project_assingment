d7c59fbf44082fb961071ac0894cedae18d31661
TANMOY <119417251+TanmoyBD@users.noreply.github.com>
2023-08-02 19:10:42 +0600

    Add files via upload

---
+++ fourth_project/first_app/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+
+def show_courses(request):
+    meals = [
+        {
+            "strMeal": "BeaverTails",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
+            "idMeal": "52928",
+            "details":"This is a korean food and verey delicious you can try this"
+        },
+        {
+            "strMeal": "Breakfast Potatoes",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+            "idMeal": "52965",
+            "details":"This is a Japanees food and verey delicious you can try this"
+        },
+        {
+            "strMeal": "Canadian Butter Tarts",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
+            "idMeal": "52923",
+            "details":"Vai tui akbar aita khaia dakh"
+        },
+        {
+            "strMeal": "Montreal Smoked Meat",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
+            "idMeal": "52927",
+            "details":"khub moja pabire"
+            
+        },
+        {
+            "strMeal": "Nanaimo Bars",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
+            "idMeal": "52924",
+            "details":"aita khaile tui amor hoia jabi"
+        },
+        {
+            "strMeal": "Pate Chinois",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
+            "idMeal": "52930",
+            "details":"akbar tui kha vai khali akba tui kha"
+        },
+        {
+            "strMeal": "Pouding chomeur",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
+            "idMeal": "52932",
+            "details":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        },
+        {
+            "strMeal": "Poutine",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+            "idMeal": "52804",
+            "details":"ki lakhtam bujteci na"
+        },
+        {
+            "strMeal": "Rappie Pie",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
+            "idMeal": "52933",
+            "details":"Thank you for see my project"
+        },
+        {
+            "strMeal": "Split Pea Soup",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
+            "idMeal": "52925",
+            "details":"abar asar jnno amontron roilo"
+        }
+    ]
+    return render(request, 'index.html', {'meals': meals})
